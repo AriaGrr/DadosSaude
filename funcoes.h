2a46042f1257a4bf8b445c15ffefbcde3138c8e1
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2024-10-21 00:12:36 -0300

    chei de coisa

--- funcoes.h
+++ funcoes.h
@@ -1,1 +1,135 @@
+// Pra que isso serve mesmo?
+#ifndef UNTITLED2_BIBLI_H
+#define UNTITLED2_BIBLI_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// #include "cjson\cJSON.h"
+#include "cJSON.h"
 
+// #include <cjson/cJSON.h>
+// #include "lib\cJSON\cJSON.c"
+// #include "cJSON.h"
+// #include "cJSON.c"
+
+// #include "funcoes.c"
+
+// Para
+#define ADD_FIELD(obj, name, type, value) \
+    cJSON_Add##type##ToObject(obj, #name, value)
+
+// Aqui devemos colocar os structs, tanto do arquivo a ser salvo quanto das estruturas que serão manipuladas
+
+// Registros de pacientes
+typedef struct
+{
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct
+{
+    char nome[50];
+    int idade;
+    char rg[10];
+    Data *entrada;
+} Registro;
+
+
+// Item de menu: Cadastrar em uma LDE
+typedef struct ELista
+{
+     Registro *dados;
+     struct ELista *proximo;
+} ELista;
+
+typedef struct
+{
+     ELista *inicio;
+     int qtde;
+} Lista;
+
+// Item de menu: Atendimento em uma Fila
+typedef struct EFila
+{
+     Registro *dados;
+     struct EFila *proximo;
+} EFila;
+
+typedef struct
+{
+     EFila *head;
+     EFila *tail;
+     int qtde;
+} Fila;
+
+// Item de menu: Pesquisa
+typedef struct EABB
+{
+     Registro *dados;
+     // Filhos esquerdo e direito
+     struct EABB *esq;
+     struct EABB *dir;
+} EABB;
+
+typedef struct
+{
+     EABB *raiz;
+     int qtde;
+} ABB;
+
+
+// typedef struct
+// {
+//     int prioridade;
+//     int estado;
+//     int categoria;
+//     char descricao[300];
+// } Tarefa;
+
+// typedef struct
+// {
+//     Tarefa tarefas[100];
+//     int qtd;
+// } ListaDeTarefas;
+
+// Aqui devemos colocar as funções que serão utilizadas do arquivo funcoes.c
+
+void clearBuffer();
+
+// void imprimirRegistro(Registro *r);
+
+cJSON* estruturaJson(void* data, const char* campo[], const char* tipo[]);
+void jsonEstrutura(const char* arquivo, void* estrutura, const char* campo[], const char* tipo[]);
+
+
+// char *estado(int e);
+// char *categoria(int c);
+
+// //Funções principais
+// int criarTarefa(ListaDeTarefas *lt);
+// int deletarTarefa(ListaDeTarefas *lt);
+// int alterarTarefa(ListaDeTarefas *lt);
+
+// // Funções para exportar
+// int exportarTarefas(ListaDeTarefas lt, char exportado[]); // Adicional, não feito.
+// int exportarPrioridade(ListaDeTarefas lt, char exportado[]);
+// int exportarCategoria(ListaDeTarefas lt, char exportado[]);
+// int exportarPCTarefa(ListaDeTarefas lt, char exportado[]);
+
+// //Funções de listagem
+// int listarTarefas(ListaDeTarefas lt);
+// int listarPrioridade(ListaDeTarefas lt);
+// int listarCategoria(ListaDeTarefas lt);
+// int listarPCTarefa(ListaDeTarefas lt);
+// int listarEstado(ListaDeTarefas lt);
+
+// void menuExportar();
+// void menuListagem();
+// void printMenu();
+// // fwrite
+// int salvarLista(ListaDeTarefas lt, char nome[]);
+// // fread
+// int carregarLista(ListaDeTarefas *lt, char nome[]);
+#endif //UNTITLED2_BIBLI_H
--- main.c
+++ main.c
@@ -1,1 +1,210 @@
+#include "funcoes.h"
+// #include "cJSON.h"
+// #include "lib\cJSON\cJSON.h"
+// #include "funcoes.c"
 
+// Alterar main pro projeto atual
+int main()
+{
+
+    Registro registro = {"João da Silva", 30, "123456789", 0};
+    registro.entrada = (Data*)malloc(sizeof(Data));
+    registro.entrada->dia = 15;
+    registro.entrada->mes = 3;
+    registro.entrada->ano = 2023;
+
+    const char *campos_registro[] = {"nome", "idade", "rg", "entrada.dia", "entrada.mes", "entrada.ano", NULL};
+    const char *tipos_registro[] = {"char*", "int", "char*", "Data", "Data", "Data", NULL};
+
+    cJSON *json = estruturaJson(&registro, campos_registro, tipos_registro);
+    char *string_json = cJSON_Print(json);
+    FILE *fp = fopen("dados.json", "w");
+    fwrite(string_json, strlen(string_json), 1, fp);
+    fclose(fp);
+    cJSON_Delete(json);
+    free(string_json);
+
+    Registro nova_registro;
+    jsonEstrutura("dados.json", &nova_registro, campos_registro, tipos_registro);
+
+    for (int i = 0; campos_registro[i] != NULL; i++)
+    {
+        printf("%s: ", campos_registro[i]);
+        if (tipos_registro[i][0] == 'c')
+        {
+            printf("%s\n", *((char **)&nova_registro + i));
+        }
+        else if (tipos_registro[i][0] == 'i')
+        {
+            printf("%d\n", *((int *)&nova_registro + i));
+        }
+        else if (tipos_registro[i][0] == 'D')
+        {
+            Data *data = (Data *)((char *)&nova_registro + i);
+            printf("%d/%d/%d\n", data->dia, data->mes, data->ano);
+        }
+    }
+    return 0;
+
+//     // Criar uma instância da estrutura Registro
+//     Registro registro = {"João da Silva", 30, "123456789", {15, 3, 2023}};
+
+//     // Criar um array de campos e tipos para a estrutura Registro
+//     const char* campos_registro[] = {"nome", "idade", "rg", "entrada.dia", "entrada.mes", "entrada.ano", NULL};
+//     const char* tipos_registro[] = {"char*", "int", "char*", "Data", "Data", "Data", NULL};
+
+//     // Converter a estrutura Registro para JSON e salvar em um arquivo
+//     cJSON* json = estruturaJson(&registro, campos_registro, tipos_registro);
+//     char* string_json = cJSON_Print(json);
+//     FILE* fp = fopen("dados.json", "w");
+//     fwrite(string_json, strlen(string_json), 1, fp);
+//     fclose(fp);
+//     cJSON_Delete(json);
+//     free(string_json);
+
+//     // Ler o arquivo JSON e popular uma nova estrutura
+//     Registro nova_registro;
+//     jsonEstrutura("dados.json", &nova_registro, campos_registro, tipos_registro);
+
+//     for (int i = 0; campos_registro[i] != NULL; i++)
+//     {
+//         printf("%s: ", campos_registro[i]);
+//         if (tipos_registro[i][0] == 'c')
+//         {
+//             printf("%s\n", *((char**) &nova_registro + i));
+//         }
+//         else if (tipos_registro[i][0] == 'i')
+//         {
+//             printf("%d\n", *((int*) &nova_registro + i));
+//         }
+//         else if (tipos_registro[i][0] == 'D')
+//         {
+//             Data* data = (Data*) ((char*) &nova_registro + i);
+//             printf("%d/%d/%d\n", data->dia, data->mes, data->ano);
+//         }
+//     }
+    // // Imprimir os dados da nova estrutura para verificar
+    // printf("Nome: %s\n", nova_registro.nome);
+    // printf("Idade: %d\n", nova_registro.idade);
+    // printf("RG: %s\n", nova_registro.rg);
+    // printf("Data de entrada: %d/%d/%d\n", nova_registro.entrada.dia, nova_registro.entrada.mes, nova_registro.entrada.ano);
+    // // ... imprimir outros campos ...
+
+//   int cod;
+  // Ia ser usado para salvar os arquivos em txt, mas irei tentar fazer em json
+//   char arquivo[] = "arquivos";
+//   char exportado[] = "-";
+//   ArquivosPacientes lt;
+
+//   cod = carregarLista(&lt, arquivo);
+//   if (cod == 1)
+//   {
+//     lt.qtd = 0;
+//   }
+//   int opcao;
+//   do
+//   {
+//     printMenu();
+//     scanf("%d", &opcao);
+//     printf("%d\n", opcao);
+//     if (opcao == 0)
+//     {
+//       printf("Saindo...\n");
+//     }
+//     else if (opcao == 1)
+//     {
+//       criarTarefa(&lt);
+//       // Limpar o terminal
+//       system("clear");
+//     }
+//     else if (opcao == 2)
+//     {
+//       deletarTarefa(&lt);
+//       system("clear");
+//     }
+//     else if (opcao == 3)
+//     {
+
+//       system("clear");
+
+//       menuListagem();
+//       int opcao2;
+//       scanf("%d", &opcao2);
+//       system("clear");
+//       if (opcao2 == 1)
+//       {
+//         listarTarefas(lt);
+//       }
+//       else if (opcao2 == 2)
+//       {
+//         listarPrioridade(lt);
+//       }
+//       else if (opcao2 == 3)
+//       {
+//         listarCategoria(lt);
+//       }
+//       else if (opcao2 == 4)
+//       {
+//         listarEstado(lt);
+//       }
+//       else if (opcao2 == 5)
+//       {
+//         listarPCTarefa(lt);
+//       }
+//       else
+//       {
+//         printf("Opção não existe!\n");
+//       }
+//       printf("Pressione Enter para voltar ao menu...");
+//       // Pausar o terminal
+//       clearBuffer();
+//       clearBuffer();
+//       system("clear");
+//     }
+//     else if (opcao == 4)
+//     {
+//       system("clear");
+//       menuExportar();
+//       int opcao3;
+//       scanf("%d", &opcao3);
+//       system("clear");
+
+//       if (opcao3 == 1)
+//       {
+//         exportarTarefas(lt, exportado);
+//       }
+//       else if (opcao3 == 2)
+//       {
+//         exportarPrioridade(lt, exportado);
+//       }
+//       else if (opcao3 == 3)
+//       {
+//         exportarCategoria(lt, exportado);
+//       }
+//       else if (opcao3 == 4)
+//       {
+//         exportarPCTarefa(lt, exportado);
+//       }
+//       else
+//       {
+//         printf("Opção não existe!\n");
+//       }
+//     }
+//     else if (opcao == 5)
+//     {
+//       system("clear");
+//       alterarTarefa(&lt);
+//     }
+//     else
+//     {
+//       printf("Opção não existe!\n");
+//     }
+//     cod = salvarLista(lt, arquivo);
+//   } while (opcao != 0);
+
+//   if (cod != 0)
+//   {
+//     printf("Erro ao salvar arquivo!\n");
+//   }
+//   return 0;
+}
